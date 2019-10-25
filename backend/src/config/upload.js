ddc7e5071c0eb1337396ad2f5c84e51347095de6
Matheus Botelho <matheus.botelho97@gmail.com>
2019-10-24 23:15:47 -0300

    second comit backend

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -3,6 +3,12 @@ const path = require('path')
 
 module.exports = {
     storage: multer.diskStorage({
-        //destination: path.resolve(__dirname, '..')
+        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        filename: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext)
+
+            cb(null, `${name}-${Date.now()}${ext}`)
+        }
     })
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController')
 const SpotsController = require('./controllers/SpotController')
+const DashboarController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express();
+const upload = multer(uploadConfig)
 
 routes.use(express.json())
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/spots', SpotsController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotsController.store)
+routes.get('/spots', SpotsController.index)
+
+routes.get('/dashboard', DashboarController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes
