ab9f0aa4beee1e9dc1923cf4acad87f83d3e4654
Matheus Botelho <matheusbc97@users.noreply.github.com>
2019-10-26 15:01:50 -0300

    second comit front-end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors')
+const path = require('path')
 
 const app = express();
 
@@ -15,7 +17,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-dvfnb.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar o corpo da requisição (para criação, edição)
 
+app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 app.listen(3333)
